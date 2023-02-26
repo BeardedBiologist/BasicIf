72203279ce33b4d61d75250e841cf52a0a855184
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-26 20:56:57 +0100

    Added extra functionality

--- Program.cs
+++ Program.cs
@@ -4,14 +4,18 @@
 
 Console.Write("What is your first name: ");
 string? firstName = Console.ReadLine();
+string? lastName;
 
 if (firstName.ToLower() == "josh")
 {
     Console.WriteLine("Hello Mr. O'Connor");
+    lastName = "O'Connor";
 }
 else
 {
     Console.WriteLine($"Hello {firstName}");
+    lastName = "Smith";
 }
 
+Console.WriteLine("Last name: " + lastName);
 Console.WriteLine("End of program.");
