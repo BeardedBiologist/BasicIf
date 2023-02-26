62fc5ab0286997fb3ae15b386197dc3d8bdd7b69
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-26 20:40:27 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write("What is your first name: ");
+string? firstName = Console.ReadLine();
+
+if (firstName == "Josh")
+{
+    Console.WriteLine("Hello Mr. O'Connor");
+}
+else
+{
+    Console.WriteLine($"Hello {firstName}");
+}
+
+Console.WriteLine("End of program");
